6b99701502be73ae1c4342a6f03ffe0b90f0b265
ssair <ssirezha@gmail.com>
2022-08-16 08:56:56 +0300

    рекурсия

---
+++ Рекурсия/generate_number.py
@@ -0,0 +1,34 @@
+def generate_bin(M:int, prefix=""):
+    """
+        Генерирует все числа (с лидирующими незначащами нулями)
+        в 2-ичной системе счисления
+        длины M
+        """
+    if M == 0:
+        print(prefix)
+        return
+    generate_bin(M-1, prefix + "0")
+    generate_bin(M-1, prefix + "1")
+
+
+def generate_number(N:int, M:int, prefix=None):
+    """
+    Генерирует все числа (с лидирующими незначащами нулями)
+    в N-ричной системе счисления (N <= 10)
+    длины M
+    """
+    prefix = prefix or []
+    if M == 0:
+        print(*prefix, sep="")
+        return
+    for digit in range(N):
+        prefix.append(digit)
+        generate_number(N, M-1, prefix)
+        prefix.pop()
+
+
+# только для двоичной СС
+generate_bin(5)
+
+# для произвольной СС
+generate_number(3, 2)
---
+++ Рекурсия/generate_permutations.py
@@ -0,0 +1,30 @@
+def find_number_in_prefix(number, A):
+    """
+    Ищет number в A и возвращает True, если такой есть
+    и False, если такого нет
+    """
+    flag = False
+    for x in A:
+        if number == x:
+            flag = True
+            break
+    return flag
+
+def generate_permutations(N:int, M:int=-1, prefix=None):
+    """
+    Генерирация всех перестановок N в M позициях
+    с префиксом prefix
+    """
+    M = N if M == -1 else M # по умолчанию N чисел в N позициях
+    prefix = prefix or []
+    if M == 0:
+        print(prefix)
+        return
+    for number in range(1, N+1):
+        if find_number_in_prefix(number, prefix):
+            continue
+        prefix.append(number)
+        generate_permutations(N, M-1, prefix)
+        prefix.pop()
+
+generate_permutations(3)
