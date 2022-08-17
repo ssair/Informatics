65abd6cd8167432f14b14c4800e74f21cf4b8370
ssair <ssirezha@gmail.com>
2022-08-17 10:08:33 +0300

    Алгоритмы

--- Рекурсия/merge_sort.py
+++ Рекурсия/merge_sort.py
@@ -1,48 +1,48 @@
 from random import randint
 
 
-def merge(A:list, B:list):
+def merge(lst_a: list, lst_b: list):
     """
     Функция для слияния 2-х отсортированных списков
     """
-    C = [0] * (len(A) + len(B))
+    lst_c = [0] * (len(lst_a) + len(lst_b))
     i = k = n = 0
-    while i < len(A) and k < len(B):
-        if A[i] <= B[k]:
-            C[n] = A[i]
+    while i < len(lst_a) and k < len(lst_b):
+        if lst_a[i] <= lst_b[k]:
+            lst_c[n] = lst_a[i]
             i += 1
             n += 1
         else:
-            C[n] = B[k]
+            lst_c[n] = lst_b[k]
             k += 1
             n += 1
 
-    while i < len(A):
-        C[n] = A[i]
+    while i < len(lst_a):
+        lst_c[n] = lst_a[i]
         i += 1
         n += 1
-    while k < len(B):
-        C[n] = B[k]
+    while k < len(lst_b):
+        lst_c[n] = lst_b[k]
         k += 1
         n += 1
 
-    return C
+    return lst_c
 
 
-def merge_sort(A:list):
+def merge_sort(lst: list):
     """
     Сортировка слиянием
     """
-    if len(A) <= 1:
+    if len(lst) <= 1:
         return
-    middle = len(A) // 2
-    L = [A[i] for i in range(0, middle)]
-    R = [A[i] for i in range(middle, len(A))]
-    merge_sort(L)
-    merge_sort(R)
-    C = merge(L, R)
-    for i in range(len(A)):
-        A[i] = C[i]
+    middle = len(lst) // 2
+    lst_l = [lst[i] for i in range(0, middle)]
+    lst_r = [lst[i] for i in range(middle, len(lst))]
+    merge_sort(lst_l)
+    merge_sort(lst_r)
+    lst_c = merge(lst_l, lst_r)
+    for i in range(len(lst)):
+        lst[i] = lst_c[i]
 
 a = [randint(0, 20) for i in range(20)]
 print(*a)
