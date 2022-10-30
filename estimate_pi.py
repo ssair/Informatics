196602857469d56e54e39673c59d9d936894341b
ssair <ssirezha@gmail.com>
2022-10-30 08:16:28 +0300

    Алгоритмы

---
+++ estimate_pi.py
@@ -0,0 +1,20 @@
+import random
+
+def estimate_pi(n):
+    num_point_circle = 0 #кол-во точек внутри круга
+    num_point_total = n
+
+    for i in range(n):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        distance = x**2 + y**2
+
+        if distance <= 1: #определяем, что точка попала внутрь круга
+            num_point_circle += 1
+
+    return 4 * num_point_circle/num_point_total
+
+n = 10000000 #общее количество точек (чем больше точек, тем лучше точность)
+pi =  estimate_pi(n)
+
+print(pi)
