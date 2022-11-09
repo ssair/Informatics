61f925fdc733185a3b26fc7128ef6df417679796
ssair <ssirezha@gmail.com>
2022-11-09 22:47:19 +0300

    Алгоритмы

---
+++ Запись и чтение в файл/текстовый_файл.py
@@ -0,0 +1,15 @@
+file = open("test.txt", "w")
+file.write("hello world")
+file.close()
+
+with open("test.txt", "w") as file:
+    file.write("hello world")
+
+lines = ["first", "second", "third"]
+with open("test.txt", "w") as file:
+    for  line in lines:
+        file.write(line + '\n')
+
+with open("test.txt", "r") as file:
+    for line in file:
+        print(line)
